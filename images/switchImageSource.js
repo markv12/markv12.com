b08ab09508a7d47083f1eedcea71d1b13458b1c8
Markv12 <swsteffes@gmail.com>
2014-02-10 14:06:57 +0900

    Added new image to image page

--- images/switchImageSource.js
+++ images/switchImageSource.js
@@ -5,7 +5,7 @@ function loadBackground() {
  	
  	var images=new Array();
 	images[0]=["http://i.imgur.com/eS6lo29.jpg"];
-     
+	images[1]=["http://i.imgur.com/YIuj3gr.jpg"];
 	$("#image").attr('src', images[string[1]]);
 	$("#imagelink").attr('href', images[string[1]]); 
 }
