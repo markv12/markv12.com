d846d028d53510ae9109a1ee0501c1f94576f256
Markv12 <swsteffes@gmail.com>
2014-02-24 17:56:35 +0900

    Created separate controller and data binding

---
+++ AngularTest/testcontroller.js
@@ -0,0 +1,11 @@
+function SimpleController($scope){
+	$scope.customers = customers=[
+		{name:'Derp Herp', city:'LA'}, 
+		{name:'Herp McGerp', city:'San Francisco'}, 
+		{name:'Scott Steffes', city:'Hirakata'},
+		{name:'Alex Hoyt', city:'Oslo'},
+		{name:'Cameron Cook', city:'Minneapolis'},
+		{name:'Heather Steffes', city:'Minneapolis'},
+		{name:'Joey Orren', city:'Sauk Rapids'}
+	];
+}
