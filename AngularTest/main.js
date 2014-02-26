04551ddb162f74cad861d85f27041e9f9916d696
Markv12 <swsteffes@gmail.com>
2014-02-26 18:53:06 +0900

    It Works!!!

--- AngularTest/main.js
+++ AngularTest/main.js
@@ -1,12 +1,17 @@
-var myApp = angular.module("myApp", []);
-myApp.factory("Data", function() {
-	return {message: "I'm data from a service!"}
+var app = angular.module("behaviorApp", []);
+
+app.directive("enter", function() {
+    return function(scope, element) {
+        element.bind("mouseenter", function() {
+            element.addClass("panel");
+        })
+    }
 })
 
-function controllerOne($scope, Data){
-	$scope.data = Data;
-}
-
-function controllerTwo($scope, Data){
-	$scope.data = Data;
-}
+app.directive("leave", function() {
+    return function(scope, element) {
+        element.bind("mouseleave", function() {
+            element.removeClass("panel");
+        })
+    }
+})
