12596b98d7b189a18f01414e17ef61359fec847d
Markv12 <swsteffes@gmail.com>
2014-05-04 02:11:43 +0900

    Added formatting

--- error.php
+++ error.php
@@ -1,47 +1,45 @@
-<?php
-
-$page_redirected_from = $_SERVER['REQUEST_URI'];  // this is especially useful with error 404 to indicate the missing page.
+<?php $page_redirected_from = $_SERVER['REQUEST_URI'];
+// this is especially useful with error 404 to indicate the missing page.
 $server_url = "http://" . $_SERVER["SERVER_NAME"] . "/";
 $redirect_url = $_SERVER["REDIRECT_URL"];
 $redirect_url_array = parse_url($redirect_url);
 $end_of_path = strrchr($redirect_url_array["path"], "/");
 
-switch(getenv("REDIRECT_STATUS"))
-{
+switch(getenv("REDIRECT_STATUS")) {
 	# "400 - Bad Request"
-	case 400:
-	$error_code = "400 - Bad Request";
-	$explanation = "The syntax of the URL submitted by your browser could not be understood.  Please verify the address and try again.";
-	$redirect_to = "";
-	break;
+	case 400 :
+		$error_code = "400 - Bad Request";
+		$explanation = "The syntax of the URL submitted by your browser could not be understood.  Please verify the address and try again.";
+		$redirect_to = "";
+		break;
 
 	# "401 - Unauthorized"
-	case 401:
-	$error_code = "401 - Unauthorized";
-	$explanation = "This section requires a password or is otherwise protected.  If you feel you have reached this page in error, please return to the login page and try again, or contact the webmaster if you continue to have problems.";
-	$redirect_to = "";
-	break;
+	case 401 :
+		$error_code = "401 - Unauthorized";
+		$explanation = "This section requires a password or is otherwise protected.  If you feel you have reached this page in error, please return to the login page and try again, or contact the webmaster if you continue to have problems.";
+		$redirect_to = "";
+		break;
 
 	# "403 - Forbidden"
-	case 403:
-	$error_code = "403 - Forbidden";
-	$explanation = "This section requires a password or is otherwise protected.  If you feel you have reached this page in error, please return to the login page and try again, or contact the webmaster if you continue to have problems.";
-	$redirect_to = "";
-	break;
+	case 403 :
+		$error_code = "403 - Forbidden";
+		$explanation = "This section requires a password or is otherwise protected.  If you feel you have reached this page in error, please return to the login page and try again, or contact the webmaster if you continue to have problems.";
+		$redirect_to = "";
+		break;
 
 	# "404 - Not Found"
-	case 404:
-	$error_code = "404 - Not Found";
-	$explanation = "The requested resource '" . $page_redirected_from . "' could not be found on this server.  Please verify the address and try again.";
-	$redirect_to = "";
-	break;
+	case 404 :
+		$error_code = "404 - Not Found";
+		$explanation = "The requested resource '" . $page_redirected_from . "' could not be found on this server.  Please verify the address and try again.";
+		$redirect_to = "";
+		break;
 
 	# "500 - Internal Server Error"
-	case 500:
-	$error_code = "500 - Internal Server Error";
-	$explanation = "The server experienced an unexpected error.  Please verify the address and try again.";
-	$redirect_to = "";
-	break;
+	case 500 :
+		$error_code = "500 - Internal Server Error";
+		$explanation = "The server experienced an unexpected error.  Please verify the address and try again.";
+		$redirect_to = "";
+		break;
 }
 ?>
 
@@ -52,19 +50,28 @@ switch(getenv("REDIRECT_STATUS"))
 
 	<link rel="Shortcut Icon" href="/favicon.ico" type="image/x-icon" />
 
-	<title>Page not found: <?php print ($server_url); ?></title>
+	<title>Page not found: <?php print($server_url); ?></title>
+	<link rel="stylesheet" href="foundation/css/foundation.min.css">
 	<link rel="stylesheet" href="stylesheet.css">
 
 </head>
 <body>
 
-<h1>Error Code <?php print ($error_code); ?></h1>
+<div class="row" style="color: #CCCCCC">
+	<div class="large-8 medium-8 columns large-centered medium-centered">
+		<div class="panel">
+			<h1>Error Code <?php print($error_code); ?></h1>
 
-<p>The <a href="http://en.wikipedia.org/wiki/Uniform_resource_locator">URL</a> you requested was not found. <?PHP echo($explanation); ?></p>
+			<p>The <a href="http://en.wikipedia.org/wiki/Uniform_resource_locator">URL</a> you requested was not found. <?PHP echo($explanation); ?></p>
 
-<p>You may also want to try starting from the home page: <a href="<?php print ($server_url); ?>"><?php print ($server_url); ?></a></p>
+			<p>You may also want to try starting from the home page: <a href="<?php print($server_url); ?>"><?php print($server_url); ?></a></p>
 
-<hr />
+			<hr />
+		</div>
+	</div>
+</div>
+
+
 
 </body>
 </html>
