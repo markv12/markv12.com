bb7e518a62e361a16470e3efddfabc6c7e86969a
Markv12 <swsteffes@gmail.com>
2014-05-10 00:24:52 +0900

    Got masonry working!

--- about/main.js
+++ about/main.js
@@ -1,8 +1,16 @@
 var container = document.querySelector('#container');
 
-// initialize Masonry
-var msnry = new Masonry( container );
-// layout Masonry again after all images have loaded
-imagesLoaded( container, function() {
-  msnry.layout();
+var msnry = new Masonry(container, {
+	// options
+	columnWidth : 200,
+	itemSelector : '.item',
+	containerStyle: null
 });
+//msnry.layout();
+
+/* layout Masonry again after all images have loaded
+imagesLoaded(container, function() {
+	msnry.layout();
+	alert("dick");
+});
+*/
